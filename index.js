dc87f9c4518c8dad0a698b37da8f695481982723
AkiTheFish <99485078+AkiTheFish@users.noreply.github.com>
2024-07-05 04:50:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import express from "express";
 const application = express();
 const port = 8080;
 
+application.set("view engine", "ejs");
+application.set("views", "./views");
+
 application.get("/", (req, res) => {
   res.render("main.ejs");
 });
