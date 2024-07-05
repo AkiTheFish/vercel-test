9106a622e1cfe28dadef6149b6b7745750ef1b62
AkiTheFish <99485078+AkiTheFish@users.noreply.github.com>
2024-07-05 04:53:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const application = express();
 const port = 8080;
 
 application.set("view engine", "ejs");
-application.set("views", "./views");
+application.set("views", path.join(__dirname, "views"));
 
 application.get("/", (req, res) => {
   res.render("main.ejs");
