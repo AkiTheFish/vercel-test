5c6d7471d7343390ef91a274a412536cf69183bf
AkiTheFish <99485078+AkiTheFish@users.noreply.github.com>
2024-07-05 04:46:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const application = express();
 const port = 8080;
 
-application.get("/", (req, res) => res.send("hi"));
+application.get("/", (req, res) => res.render("main.ejs"));
 
 application.listen(port, () =>
   console.log(`succeeded connection to port: ${port}`)
